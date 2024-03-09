7791250a4ac92d12028a2d8d340af08b181814e8
AlexLexicon <Alexrs.business@outlook.com>
2024-03-08 23:52:25 -0800

    added missing extenion methods for service registations

---
+++ Lexicom.Concentrate.Blazor.WebAssembly.Amenities/Extensions/ConcentrateBlazorWebAssemblyServiceBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Lexicom.Concentrate.Supports.Blazor.WebAssembly;
+
+namespace Lexicom.Concentrate.Blazor.WebAssembly.Amenities.Extensions;
+public static class ConcentrateBlazorWebAssemblyServiceBuilderExtensions
+{
+    /// <exception cref="ArgumentNullException"/>
+    public static IConcentrateBlazorWebAssemblyServiceBuilder AddAmenities(this IConcentrateBlazorWebAssemblyServiceBuilder builder)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+
+        builder.WebAssemblyHostBuilder.Services.AddLexicomConcentrateBlazorWebAssemblyAmenities();
+
+        return builder;
+    }
+}
---
+++ Lexicom.Concentrate.Blazor.WebAssembly.Amenities/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Lexicom.Concentrate.Blazor.WebAssembly.Amenities.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lexicom.Concentrate.Blazor.WebAssembly.Amenities.Extensions;
+public static class ServiceCollectionExtensions
+{
+    /// <exception cref="ArgumentNullException"/>
+    public static IServiceCollection AddLexicomConcentrateBlazorWebAssemblyAmenities(this IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        services.AddSingleton<IBrowserService, BrowserService>();
+        services.AddSingleton<INavigationService, NavigationService>();
+
+        return services;
+    }
+}
