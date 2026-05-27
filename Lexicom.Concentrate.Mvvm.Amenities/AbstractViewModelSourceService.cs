c9c9ad8004684baf8440b4ec8ded723eb3929e0e
AlexLexicon <Alexrs.business@outlook.com>
2026-05-27 09:05:23 -0700

    made the ViewModel access from the viewmodelsourceservices into methods and renamed the classes

--- Lexicom.Concentrate.Mvvm.Amenities/AbstractViewModelSourceService.cs
+++ Lexicom.Concentrate.Mvvm.Amenities/AbstractViewModelSourceService.cs
@@ -2,19 +2,19 @@
 
 namespace Lexicom.Concentrate.Mvvm.Amenities;
 
-public abstract class AbstractSingletonViewModelSourceService<TViewModel> where TViewModel : class
+public abstract class AbstractViewModelSourceService<TViewModel> where TViewModel : class
 {
     private readonly IViewModelProvider<TViewModel> _viewModelProvider;
 
     /// <exception cref="ArgumentNullException"/>
-    protected AbstractSingletonViewModelSourceService(IViewModelProvider<TViewModel> viewModelProvider)
+    protected AbstractViewModelSourceService(IViewModelProvider<TViewModel> viewModelProvider)
     {
         ArgumentNullException.ThrowIfNull(viewModelProvider);
 
         _viewModelProvider = viewModelProvider;
     }
 
-    public TViewModel? ViewModel => _viewModelProvider
+    public TViewModel? GetViewModel() => _viewModelProvider
         .GetViewModels()
         .SingleOrDefault();
 }
--- Lexicom.Concentrate.Mvvm.Amenities/AbstractViewModelsSourceService.cs
+++ Lexicom.Concentrate.Mvvm.Amenities/AbstractViewModelsSourceService.cs
@@ -2,17 +2,17 @@
 
 namespace Lexicom.Concentrate.Mvvm.Amenities;
 
-public abstract class AbstractTransientViewModelSourceService<TViewModel> where TViewModel : class
+public abstract class AbstractViewModelsSourceService<TViewModel> where TViewModel : class
 {
     private readonly IViewModelProvider<TViewModel> _viewModelProvider;
 
     /// <exception cref="ArgumentNullException"/>
-    protected AbstractTransientViewModelSourceService(IViewModelProvider<TViewModel> viewModelProvider)
+    protected AbstractViewModelsSourceService(IViewModelProvider<TViewModel> viewModelProvider)
     {
         ArgumentNullException.ThrowIfNull(viewModelProvider);
 
         _viewModelProvider = viewModelProvider;
     }
 
-    public IReadOnlyList<TViewModel> ViewModels => _viewModelProvider.GetViewModels();
+    public IReadOnlyList<TViewModel> GetViewModels() => _viewModelProvider.GetViewModels();
 }
